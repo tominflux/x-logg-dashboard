3b00ad9d3afd4b9ba6d8656cfbaf8e1101d57e4a
Tom <tom.chesters96@gmail.com>
2020-08-29 22:38:03 +0100

    Styled login page

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -1,35 +1,69 @@
+import styles from './login.module.css'
 
 
 const Login = () => {
+    console.log(styles)
     //Render
     return (
-        <div className="login">
-            <div className="row">
-                <div className="col-md-6 login__label-col">
-                    Username:
+        <div 
+            className={styles.login}
+        >
+            <div className="row align-items-center">
+                <div 
+                    className={
+                        `col-md-5 ${styles.login__labelCol}`
+                    }
+                >
+                    <span
+                        className={styles.login__label}
+                    >
+                        Username:
+                    </span>
                 </div>
-                <div className="col-md-6 login__input-col">
+                <div
+                    className={
+                        `col-md-7 ${styles.login__inputCol}`
+                    }
+                >
                     <input
                         className="login__input"
                         type="text"
                     />
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-6 login__label-col">
-                    Password:
+            <div className="row align-items-center">
+                <div 
+                    className={
+                        `col-md-5 ${styles.login__labelCol}`
+                    }
+                >
+                    <span
+                        className={styles.login__label}
+                    >
+                        Password:
+                    </span>
                 </div>
-                <div className="col-md-6 login__input-col">
+                <div
+                    className={
+                        `col-md-7 ${styles.login__inputCol}`
+                    }
+                >
                     <input
-                        className="login__input"
+                        className={styles.login__input}
                         type="password"
                     />
                 </div>
             </div>
-            <div className="row">
-                <div className="col login__submit-col">
+            <div className="row align-items-center">
+                <div 
+                    className={
+                        `col ${styles.login__submitCol}`
+                    }
+                >
                     <button
-                        className="btn btn-primary"
+                        className={
+                            `btn btn-primary ${styles.login__submit}`
+                        }
                     >
                         Log In
                     </button>
