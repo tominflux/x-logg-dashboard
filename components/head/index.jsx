c8bf947fa3ba5b25ca06f5b4032f9762cf440686
Tom <tom.chesters96@gmail.com>
2020-08-31 17:01:56 +0100

    Implemented login redirect

--- components/head/index.jsx
+++ components/head/index.jsx
@@ -1,40 +1,59 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useSelector } from 'react-redux'
+import { AUTH_STATE } from '../../redux/reducers/auth'
 import styles from './head.module.css'
 
-const DashboardHead = () => (<>
-    <Head>
-        <title>
-            Dashboard - [Sitename]
-        </title>
-        <link 
-            rel="stylesheet" 
-            href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
-            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" 
-            crossOrigin="anonymous"
-        />
-    </Head>
-    <header className={styles.header}>
-        <div className="row align-items-center">
-            <div className="col-md-3 text-center">
-                [Icon]
+const DashboardHead = () => {
+    //Hooks
+    const router = useRouter()
+    const authState = useSelector(state => state.Auth.authState)
+    //Effects
+    React.useEffect(() => {
+        const shouldRedirect = (
+            router.pathname !== "/login" &&
+            authState !== AUTH_STATE.LOGGED_IN
+        )
+        if (shouldRedirect) {
+            router.push("/login")
+        }
+    })
+    //Render
+    return (<>
+        <Head>
+            <title>
+                Dashboard - [Sitename]
+            </title>
+            <link 
+                rel="stylesheet" 
+                href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
+                integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" 
+                crossOrigin="anonymous"
+            />
+        </Head>
+        <header className={styles.header}>
+            <div className="row align-items-center">
+                <div className="col-md-3 text-center">
+                    [Icon]
+                </div>
+                <div className="col-md-3 text-center">
+                    <h1 className={styles.header__heading}>
+                        [Sitename] Dashboard
+                    </h1>
+                </div>
+                <div className="col-md-3">
+                    <span className={styles.header__welcome}>
+                        Welcome [Admin Name]
+                    </span>
+                </div>
+                <div className="col-md-3 text-center">
+                    <span className={styles.header__logout}>
+                        [Log-Out]
+                    </span>
+                </div>
             </div>
-            <div className="col-md-3 text-center">
-                <h1 className={styles.header__heading}>
-                    [Sitename] Dashboard
-                </h1>
-            </div>
-            <div className="col-md-3">
-                <span className={styles.header__welcome}>
-                    Welcome [Admin Name]
-                </span>
-            </div>
-            <div className="col-md-3 text-center">
-                <span className={styles.header__logout}>
-                    [Log-Out]
-                </span>
-            </div>
-        </div>
-    </header>
-</>)
+        </header>
+    </>)
+}
 
 export default DashboardHead
