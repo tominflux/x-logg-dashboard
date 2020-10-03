bccaaa045b28300727c116939b91d49c78e640a1
Tom Chesters <tom@tomaaato.xyz>
2020-10-03 04:55:40 +0100

    Catalogue Header

--- components/catalogues/header/index.jsx
+++ components/catalogues/header/index.jsx
@@ -1,17 +1,51 @@
 import React from 'react'
 import DeleteCatalogue from './delete'
-import CataloguesPicker from './picker'
+import CataloguePicker from './picker'
+import { useSelector, useDispatch } from 'react-redux'
+import { getCatalogues } from '../../../requests/catalogue'
+import CATALOGUE_ACTIONS from '../../../redux/actions/catalogue'
 //import PropTypes from 'prop-types'
 
-const CataloguesHeader = () => (
-    <div className="row">
-        <div className="col-8">
-            <CataloguesPicker />
+const CataloguesHeader = () => {
+    //Hooks
+    const {
+        //fetchingCatalogueNames,
+        catalogueNames,
+        //selectedCatalogueName
+    } = useSelector(state => state.Catalogue)
+    const dispatch = useDispatch()
+    //Effects
+    // - Fetch Catalogue Names
+    React.useEffect(() => {
+        const retrieveCatalogueNames = async () => {
+            try {
+                const catalogueNames = await getCatalogues()
+                const receiveCatalogueNames =
+                    CATALOGUE_ACTIONS.receiveCatalogueNames(catalogueNames)
+                dispatch(receiveCatalogueNames)
+            } catch (err) {
+                const fetchCatalogueNamesFailed =
+                    CATALOGUE_ACTIONS.fetchCatalogueNamesFailed(err)
+                dispatch(fetchCatalogueNamesFailed)
+            }
+        }
+        if (catalogueNames === null) {
+            retrieveCatalogueNames()
+        }
+    })
+    //Render
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-7 text-center">
+                    <CataloguePicker />
+                </div>
+                <div className="col-5">
+                    <DeleteCatalogue />
+                </div>
+            </div>
         </div>
-        <div className="col-4">
-            <DeleteCatalogue />
-        </div>
-    </div>
-)
+    )
+}
 
 export default CataloguesHeader
