817ab1577f757c7cb98b61ef5ed1233576c871f6
Tom Chesters <tom@tomaaato.xyz>
2020-10-03 04:57:23 +0100

    Add theme css to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,8 +5,9 @@ import AuthCheck from '../components/authCheck'
 import AppContainer from '../components/appContainer'
 import { useStore } from '../redux/store'
 import Head from 'next/head'
+import '../styles/theme.css'
 
-const DashboardApp = ({Component, pageProps}) => {
+const DashboardApp = ({ Component, pageProps }) => {
     const store = useStore(pageProps.initialReduxState)
     //Render
     return (
@@ -15,10 +16,10 @@ const DashboardApp = ({Component, pageProps}) => {
                 <title>
                     Dashboard - [Sitename]
                 </title>
-                <link 
-                    rel="stylesheet" 
-                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
-                    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" 
+                <link
+                    rel="stylesheet"
+                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+                    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
                     crossOrigin="anonymous"
                 />
             </Head>
@@ -31,7 +32,7 @@ const DashboardApp = ({Component, pageProps}) => {
 }
 
 DashboardApp.propTypes = {
-    Component: PropTypes.node,
+    Component: PropTypes.func,
     pageProps: PropTypes.object
 }
 
