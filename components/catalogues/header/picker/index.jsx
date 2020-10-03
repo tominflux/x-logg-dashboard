036bf435cb803a181192f45a9154369d5cb964c5
Tom Chesters <tom@tomaaato.xyz>
2020-10-03 02:11:11 +0100

    Catalogue Picker Redux Integration

--- components/catalogues/header/picker/index.jsx
+++ components/catalogues/header/picker/index.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import CATALOGUE_ACTIONS from '../../../../redux/actions/catalogue'
 
 const CataloguesPicker = () => {
+    //Hooks
+    const {
+        //fetchingCatalogueNames,
+        catalogueNames,
+        selectedCatalogueName
+    } = useSelector(state => state.Catalogue)
+    const dispatch = useDispatch()
     //State
     const [isOpen, setOpen] = React.useState(false)
     //Events
     const onToggleClick = () => setOpen(!isOpen)
-    const onCatalogueClick = (catalogueName) => { }
+    const onCatalogueClick = (catalogueName) => {
+        const selectCatalogue = CATALOGUE_ACTIONS.selectCatalogue(catalogueName)
+        dispatch(selectCatalogue)
+    }
     //Computations
     const rootClassName = `dropdown${isOpen ? " show" : ""}`
+    const isDisabled = (catalogueNames === null && catalogueNames.length > 0)
+    //Effects
+    // - If none selected, select first.
+    React.useEffect(() => {
+        if (!isDisabled && selectedCatalogueName === null) {
+            const firstCatalogue = catalogueNames[0]
+            const selectCatalogue = CATALOGUE_ACTIONS.selectCatalogue(firstCatalogue)
+            dispatch(selectCatalogue)
+        }
+    })
     //Render
     return (
         <div className={rootClassName} onClick={() => onToggleClick()}>
             <button
                 className="btn btn-primary dropdown-toggle"
                 type="button"
+                disabled={isDisabled}
             >
                 {selectedCatalogueName}
             </button>
@@ -29,9 +53,6 @@ const CataloguesPicker = () => {
                         </span>
                     ))
                 }
-                <span className="dropdown-item">Action</span>
-                <span className="dropdown-item">Another action</span>
-                <span className="dropdown-item">Something else here</span>
             </div>
         </div>
     )
